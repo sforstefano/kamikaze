99a71cd52b72ae641c9d703676fd3e71a6b1a09e
Stefano Cattoni <stefanocattoni98@gmail.com>
2017-06-08 09:37:14 +0200

    Vista settings d euser, solo muestra informacion del usuario

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,30 @@
 class SettingsController < ApplicationController
 	layout 'admin'
+
   def index
+  	@setting = current_user.setting
+  end
+
+  def show
+  	@setting = current_user.setting
+  end
+
+  def new
+  	@setting = current_user.setting.new
+  end
+
+  def create
+  	@setting = current_user.setting.new(setting_params)
+  	if @setting.save
+  		redirect_to settings_path
+  	else
+  		render :new
+  	end
+
+  	private
+
+  	def setting_params
+  		params.require(:setting).permit(:language, :country)
+  	end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,9 +18,5 @@ end
 
 resources :settings
 
-
-
-
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
